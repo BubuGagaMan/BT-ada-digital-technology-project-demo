f03ce822f4c00f2180ca964b6a170232eb628e9a
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-09-03 21:38:01 +0100

    Simple example of embedding a google form

---
+++ src/GoogleForm.tsx
@@ -0,0 +1,17 @@
+export default function GoogleForm() {
+    return (
+        <div className="flex justify-center p-4">
+            <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSepGcAPacis7p92VFBY2fZf9ErjHcvpemq_-QZL2HBt6_1F2Q/viewform?embedded=true"
+                width="640"
+                height="451"
+                // frameBorder="0"
+                // marginHeight="0"
+                // marginWidth="0"
+            >
+                Loading…
+            </iframe>
+            ;
+        </div>
+    );
+}
