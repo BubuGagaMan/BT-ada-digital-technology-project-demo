bb42112ef8d39e65216bb8f4e3ed3fa7bd75cf1f
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-09-03 22:15:26 +0100

    basic embeded example

--- src/GoogleForm.tsx
+++ src/GoogleForm.tsx
@@ -1,17 +1,13 @@
-export default function GoogleForm() {
-    return (
-        <div className="flex justify-center p-4">
-            <iframe
-                src="https://docs.google.com/forms/d/e/1FAIpQLSepGcAPacis7p92VFBY2fZf9ErjHcvpemq_-QZL2HBt6_1F2Q/viewform?embedded=true"
-                width="640"
-                height="451"
-                // frameBorder="0"
-                // marginHeight="0"
-                // marginWidth="0"
-            >
-                Loading…
-            </iframe>
-            ;
-        </div>
-    );
+export default function GogleForm() {
+  return (
+    <form
+      action="https://docs.google.com/forms/d/e/1FAIpQLSepGcAPacis7p92VFBY2fZf9ErjHcvpemq_-QZL2HBt6_1F2Q/formResponse"
+      method="POST"
+      target="_blank"
+    >
+      <input type="text" name="entry.2140127032" placeholder="Type here" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
+
